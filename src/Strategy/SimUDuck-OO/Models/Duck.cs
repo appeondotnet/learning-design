a9f2099c312f993458c00fd030e9bd2eb88c7127
codeping <deping.du@outlook.com>
2020-07-05 12:44:52 +0800

    Duck 模拟程序初版。

---
+++ src/Strategy/SimUDuck-OO/Models/Duck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimUDuck.Models
+{
+    public abstract class Duck
+    {
+        public void Quack()
+        {
+            Console.WriteLine("Duck: Quack.");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("Duck: Swim.");
+        }
+
+        public abstract void Display();
+    }
+}
---
+++ src/Strategy/SimUDuck-OO/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using SimUDuck.Models;
+
+namespace SimUDuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NormalTest();
+
+            Console.ReadKey();
+        }
+
+        static void NormalTest()
+        {
+            Duck duck;
+
+            duck = new MallardDuck();
+            call();
+
+            duck = new ReadheadDuck();
+            call();
+
+            void call()
+            {
+                duck.Swim();
+                duck.Quack();
+                duck.Display();
+            }
+        }
+    }
+}
