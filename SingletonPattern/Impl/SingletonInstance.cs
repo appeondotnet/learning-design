02a3d20f28c5c6f53629fdb9904cfe9d6a59be42
pengsongkun741@gmail.com <a5885516>
2020-07-06 22:30:31 +0800

    单例模式案例

---
+++ SingletonPattern/Impl/SingletonInstance.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern.Impl
+{
+    internal class SingletonInstance
+    {
+        private static volatile SingletonInstance _instance;
+        private readonly static object _lock = new object();
+
+        private SingletonInstance()
+        {
+
+        }
+
+        public SingletonInstance GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SingletonInstance();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+    }
+}
