c3be77c4a0a0a9c976bcd198891bf1035d448325
GreenShadeZhang <gil.zhang.dev@outlook.com>
2020-08-30 22:51:22 +0800

    添加模板方法模式示例

---
+++ src/TemplateMethod/TemplateMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass c;
+            c = new ConcreteClassA();
+            c.TemplateMethod();
+            c = new ConcreteClassB();
+            c.TemplateMethod();
+            Console.Read();
+        }
+    }
+}
